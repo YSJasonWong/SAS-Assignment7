bfc54412aee508196f852bf5261398c71af996cd
Jason Wong <73357612+YSJasonWong@users.noreply.github.com>
2022-03-10 17:14:39 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+let n = 9, gramArray = {};
+let button1;
+
+function setup() {
+  for (let i = 0; i<= inputText.length-n; i++) {
+    let gram = inputText.substring(i, i+n);
+    if (!gramArray[gram]) {
+      gramArray[gram] = [];
+    }
+    gramArray[gram].push(inputText.charAt(i+n));
+    
+  }
+  button1 = createButton("generate");
+  button1.position(50, 50);
+  button1.mousePressed(generate);
+  console.log(gramArray);
+}
+
+function generate() {
+  let current = inputText.substring(0, n);
+  let result = current;
+  for (let i = 0; i< inputText.length; i++) {
+    let possibilities = gramArray[current];
+    let next = random(possibilities);
+    result += next;
+    current = result.substring(result.length-n, result.length);
+  }
+  createP(result);
+}
